f9e239e4a46e3d3472d23ebce8339b2d63daf62d
jessa602 <jessa602@gmail.com>
2023-06-16 10:37:41 -0700

    2nd tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-      <div>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
-
 export default App
